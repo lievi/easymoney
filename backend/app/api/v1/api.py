8cbcecb995416c27ec4919fb312fe3ff1dd15559
Lievi Silva <lievi.silva@luizalabs.com>
2020-04-26 00:57:18 -0300

    First Commit

---
+++ backend/app/api/v1/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from .endpoints import utils
+
+
+api_router = APIRouter()
+api_router.include_router(utils.router, tags=["utils"])
---
+++ backend/app/api/v1/endpoints/utils.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/ping")
+async def ping() -> str:
+    """
+        Heathcheck endpoint
+    """
+    return "pong"
