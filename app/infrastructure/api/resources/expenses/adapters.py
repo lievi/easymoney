e7575fa9e7babcd8b1962fab7294ea59965dbd3d
Lievi Silva <lievi.silva@luizalabs.com>
2020-07-07 22:03:51 -0300

    refactor: moving the adapters to anothe location

---
+++ app/infrastructure/api/resources/expenses/adapters.py
@@ -0,0 +1,6 @@
+class ExpenseCreateAdapter:
+    def from_entity(self, expense):
+        pass
+
+    def to_entity(self, expense_create):
+        pass
--- app/infrastructure/db/repositories/expense_repository.py
+++ app/infrastructure/db/repositories/expense_repository.py
@@ -4,26 +4,24 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from adapters.expenses import db_model_to_entity
-from entities.expenses import Expense as ExpenseEntity
+from entities.expenses import Expense as Expense
 from interfaces.repositories.expense_repository import (
     AbstractExpenseRepository,
 )
-from infrastructure.db.models.expense import Expense
+from infrastructure.db.models.expense import Expense as ExpenseDB
 
 
 class ExpenseRepository(AbstractExpenseRepository):
     def __init__(self, db: Session):
         self.db = db
-        self.expense_model = Expense
+        self.expense_model = ExpenseDB
 
-    def create_expense(self, expense: ExpenseEntity) -> None:
-        # TODO: Remove this comments later
-
+    def create_expense(self, expense: Expense) -> None:
         # creating an json with the entity
         obj_in_data = jsonable_encoder(expense)
 
         # Instantiating the base sqlalquemy class
-        db_obj = Expense(**obj_in_data)
+        db_obj = ExpenseDB(**obj_in_data)
 
         # Putting into the database
         self.db.add(db_obj)
@@ -31,7 +29,7 @@ class ExpenseRepository(AbstractExpenseRepository):
         self.db.refresh(db_obj)
         return db_obj
 
-    def get_expense_by_id(self, id: Any) -> ExpenseEntity:
+    def get_expense_by_id(self, id: Any) -> Expense:
         expense_from_db = (
             self.db.query(self.expense_model)
             .filter(self.expense_model.id == id)
