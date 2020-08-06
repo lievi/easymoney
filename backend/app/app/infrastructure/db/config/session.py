1e1e99984edcf38ed2a219a7e2b0e1f18b2a5c45
Lievi Silva <lievi.silva@gmail.com>
2020-08-05 21:07:37 -0300

    Get database url from env var

--- backend/app/app/infrastructure/db/config/session.py
+++ backend/app/app/infrastructure/db/config/session.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+import os
 
 # TODO: Set this (and the timeout) on settings
-SQLALCHEMT_DATABASE_URL = "postgresql://postgres:t3st3123@db/easy_money"
+# SQLALCHEMT_DATABASE_URL = "postgresql://postgres:t3st3123@db/easy_money"
+SQLALCHEMT_DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine = create_engine(
     SQLALCHEMT_DATABASE_URL,
