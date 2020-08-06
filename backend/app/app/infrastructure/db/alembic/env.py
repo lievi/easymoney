e8126fbcd4f97094443c347aac8e47d01c813c1d
Lievi Silva <lievi.silva@gmail.com>
2020-08-05 21:15:30 -0300

    Modify alembic url to use env var

--- backend/app/app/infrastructure/db/alembic/env.py
+++ backend/app/app/infrastructure/db/alembic/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -27,7 +29,7 @@ target_metadata = Base.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
+config.set_main_option('sqlalchemy.url', os.environ.get('DATABASE_URL'))
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
