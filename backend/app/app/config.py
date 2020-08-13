53e4c5bd1033393a6ff4430b723d7abf5c844a96
Lievi Silva <lievi.silva@gmail.com>
2020-08-12 22:03:06 -0300

    fixup! Starting using pydantic's settings management

--- backend/app/app/config.py
+++ backend/app/app/config.py
@@ -1,4 +1,5 @@
-from pydantic import BaseSettings, PostgresDsn
+from typing import Any, Dict, Optional
+from pydantic import BaseSettings, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
@@ -6,8 +7,22 @@ class Settings(BaseSettings):
     POSTGRES_PASSWORD: str
     DATABASE_SERVER: str
     DATABASE_DB: str
-    DATABASE_URL: str = f"postgres://{POSTGRES_USER}:t3st3123@db/easy_money"
+    DATABASE_URL: Optional[PostgresDsn] = None
     DATABASE_TIMEOUT: int
+
+    @validator("DATABASE_URL", pre=True)
+    def create_postgres_connection_dns(
+        cls, v: Optional[str], values: Dict[str, Any]
+    ) -> Any:
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get('POSTGRES_USER'),
+            password=values.get('POSTGRES_PASSWORD'),
+            host=values.get('DATABASE_SERVER'),
+            path=f"/{values.get('DATABASE_DB') or ''}"
+        )
 
     class Config:
         env_file = ".env"
--- backend/app/app/infrastructure/db/config/session.py
+++ backend/app/app/infrastructure/db/config/session.py
@@ -3,10 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 
-SQLALCHEMT_DATABASE_URL = settings.DATABASE_URL
-
 engine = create_engine(
-    SQLALCHEMT_DATABASE_URL,
+    settings.DATABASE_URL,
     pool_pre_ping=True,
     connect_args={"connect_timeout": settings.DATABASE_TIMEOUT},
 )
